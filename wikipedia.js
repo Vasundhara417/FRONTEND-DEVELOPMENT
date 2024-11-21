08de98cb001a02922c39c084896b560681c322dc
Chemudupati Venkata Vasundhara <146369617+Vasundhara417@users.noreply.github.com>
2024-11-21 16:38:22 +0530

    Add files via upload

---
+++ wikipedia.js
@@ -0,0 +1,78 @@
+let searchInputEl = document.getElementById("searchInput");
+let searchResultsEl = document.getElementById("searchResults");
+let spinnerEl = document.getElementById("spinner");
+
+function createAndAppendSearchResult(result) {
+    let {
+        link,
+        title,
+        description
+    } = result;
+    //Creating Result Item  1. Div Container == result-item
+    let resultItemEl = document.createElement("div");
+    resultItemEl.classList.add("result-item");
+
+
+    //Creating Title Element Anchor title == result-title
+    let resultTitleEl = document.createElement("a");
+    resultTitleEl.classList.add("result-title");
+    resultTitleEl.textContent = title;
+    resultTitleEl.href = link;
+    resultTitleEl.target = "_blank";
+    resultItemEl.appendChild(resultTitleEl);
+
+    //Creating Break Element 
+    let titleBreakEl = document.createElement("br");
+    resultItemEl.appendChild(titleBreakEl);
+
+    //Creating URL Element Anchor URL = result-url
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+    resultItemEl.appendChild(urlEl);
+
+    //Creating Break Element 
+    let lineBreakEl = document.createElement("br");
+    resultItemEl.appendChild(lineBreakEl);
+
+    //Creating Description Element  Description -- link-description
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("link-description");
+    descriptionEl.textContent = description;
+    resultItemEl.appendChild(descriptionEl);
+
+    searchResultsEl.appendChild(resultItemEl);
+}
+
+function displayResults(searchResults) {
+    spinnerEl.classList.toggle("d-none");
+    for (let result of searchResults) {
+        createAndAppendSearchResult(result);
+    }
+}
+
+function searchWikipedia(event) {
+    if (event.key === "Enter") {
+        spinnerEl.classList.toggle("d-none");
+        searchResultsEl.textContent = "";
+        let searchInput = searchInputEl.value;
+        let url = "https://apis.ccbp.in/wiki-search?search=" + searchInput;
+        let options = {
+            method: "GET"
+        };
+        fetch(url, options)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                let {
+                    search_results
+                } = jsonData;
+                displayResults(search_results);
+            });
+    }
+}
+
+searchInputEl.addEventListener("keydown", searchWikipedia);
